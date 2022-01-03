434f5ebc9794e4cd6ae38948468cbb9a2aab4af2
Abel <adavid9988@gmail.com>
2022-01-03 14:26:42 -0800

    refactor done in the category table

--- src/main/java/com/clothingapp/clothingapp/model/Category.java
+++ src/main/java/com/clothingapp/clothingapp/model/Category.java
@@ -7,12 +7,15 @@ import java.util.List;
 @Entity
 @Table(name = "categories")
 public class Category {
+
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String name;
+
     @Column
     private String description;
 
@@ -25,33 +28,32 @@ public class Category {
     public Category() {
     }
 
-    @Column
+
     public Long getId() {
         return id;
     }
-
-    @Column
-    public String getName() {
-        return name;
-    }
-
-    @Column
-    public String getDescription() {
-        return description;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
     public void setName(String name) {
         this.name = name;
     }
 
-    @Column
+    public String getDescription() {
+        return description;
+    }
     public void setDescription(String description) {
         this.description = description;
     }
+
+
+
+
+
 }
 
 
