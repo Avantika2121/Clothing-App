2c5f59b2d10c1a38a05bf66fa864fa6474fa7069
Avantika <singh_avantika@yahoo.com>
2022-01-05 11:40:17 -0600

    Seasons service completed

--- src/main/java/com/clothingapp/clothingapp/controller/SeasonsController.java
+++ src/main/java/com/clothingapp/clothingapp/controller/SeasonsController.java
@@ -21,24 +21,24 @@ public class SeasonsController {
         this.seasonsService = seasonsService;
     }
 
-//    //Getting all the items within the category
-//    @GetMapping("/seasons/{seasonsId}")
-//    public List<Item> getSeasonItems(@PathVariable Long seasonsId) {
-//        LOGGER.info("calling getSeasonsItem from SeasonsController");
-//        return seasonsService.getSeasonsItems(seasonsId);
-//    }
-//    //Creating a season
-//    @PostMapping("/seasons")
-//    public Seasons createSeason(@RequestBody Seasons seasonsObject) {
-//        LOGGER.info("calling createSeason from SeasonsController");
-//        return seasonsService.createSeason(seasonsObject);
-//    }
-//    //Allowing the user to create an item from the seasons page. When this is done the item will automatically
-//    //belong to that specific season
-//    @PostMapping("/seasons/{seasonsId}/items")
-//    public Item createItemFromGender (@PathVariable Long seasonsId, @RequestBody Item itemObject) {
-//        LOGGER.info("calling createItemFromGender from GenderController");
-//        return seasonsService.createItemFromGender(seasonsId, itemObject);
-//    }
+    //Getting all the items within the category
+    @GetMapping("/seasons/{seasonsId}")
+    public List<Item> getSeasonItems(@PathVariable Long seasonsId) {
+        LOGGER.info("calling getSeasonsItem from SeasonsController");
+        return seasonsService.getSeasonsItems(seasonsId);
+    }
+    //Creating a season
+    @PostMapping("/seasons")
+    public Seasons createSeason(@RequestBody Seasons seasonsObject) {
+        LOGGER.info("calling createSeason from SeasonsController");
+        return seasonsService.createSeason(seasonsObject);
+    }
+    //Allowing the user to create an item from the seasons page. When this is done the item will automatically
+    //belong to that specific season
+    @PostMapping("/seasons/{seasonsId}/items")
+    public Item createItemFromSeasons (@PathVariable Long seasonsId, @RequestBody Item itemObject) {
+        LOGGER.info("calling createItemFromSeasons from SeasonsController");
+        return seasonsService.createItemFromSeasons(seasonsId, itemObject);
+    }
 
 }
