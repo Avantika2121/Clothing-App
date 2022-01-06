0f2cc7642026b3b27c83af40fd90376e8ce3737f
Abel <adavid9988@gmail.com>
2022-01-06 14:03:55 -0800

    Triple filter for items done

--- src/main/java/com/clothingapp/clothingapp/repository/ItemRepository.java
+++ src/main/java/com/clothingapp/clothingapp/repository/ItemRepository.java
@@ -1,12 +1,17 @@
 package com.clothingapp.clothingapp.repository;
 
+import com.clothingapp.clothingapp.model.Category;
+import com.clothingapp.clothingapp.model.Gender;
 import com.clothingapp.clothingapp.model.Item;
+import com.clothingapp.clothingapp.model.Seasons;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
-
     Optional<Item> findByName (String name);
+    //Optional<List<Item>> findAllByCategoryAndGenderAndSeasons (Category category, Gender gender, Seasons seasons);
+    Optional<List<Item>> findAllByCategoryIdAndGenderIdAndSeasonsId (Long categoryId, Long genderId, Long seasonsId);
+
 }
